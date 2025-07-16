eb52f7a16c4a3702ef56d1bd1e1c275aabf8203f
Abraham Brookes <abrahambrookes@gmail.com>
2025-07-16 11:31:01 +0000

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,10 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
