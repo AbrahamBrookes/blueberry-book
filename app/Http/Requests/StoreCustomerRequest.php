254d6208f4b284bddfc184d75bca23f6d52dced0
Abraham Brookes <abrahambrookes@gmail.com>
2025-07-16 14:05:53 +0000

    scaffold customer store and update requests

---
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'reference' => 'required|string|max:255',
+            'customer_category_id' => 'required|exists:customer_categories,id',
+            'started_at' => 'required|date',
+            'description' => 'nullable|string',
+        ];
+    }
+}
